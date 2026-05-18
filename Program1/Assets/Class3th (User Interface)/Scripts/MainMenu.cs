55541d469b6d5e1821735401cf2050ea22dc0cfa
SP-MICOM <superfamicom1022@gmail.com>
2026-05-18 21:43:59 +0900

    (2026.5.18) 수출용 패키지

---
+++ Program1/Assets/Class3th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        Debug.Log("StartNewGame");
+    }
+
+    public void Continue()
+    {
+        Debug.Log("Continue");
+    }
+
+    public void OpenOption()
+    {
+        Debug.Log("Option");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
