74b28431b334cc22ccd98a9251b5df9361f67411
SP-MICOM <superfamicom1022@gmail.com>
2026-05-22 21:43:45 +0900

    (2026.5.22) 시간 시스템

--- Program1/Assets/Class5th (Animator Controller)/Scripts/AnimatorManager.cs
+++ Program1/Assets/Class5th (Animator Controller)/Scripts/AnimatorManager.cs
@@ -3,24 +3,63 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     public Animator animator;
+    public AnimatorStateInfo animatorStateInfo;
 
     public void Die()
     {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animator.IsInTransition(0) || animatorStateInfo.IsName("Die"))
+        {
+            return;
+        }
+
         animator.SetTrigger("Die");
     }
 
     public void Attack()
     {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if(animatorStateInfo.IsName("Walk"))
+        {
+            animator.SetBool("Walk", false);
+        }
+        else if(animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Idle", false);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+        }
+
         animator.SetBool("Attack", !animator.GetBool("Attack"));
     }
 
     public void Walk()
     {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if(animatorStateInfo.IsName("Attack"))
+        {
+            animator.SetBool("Attack", false);
+        }
+        else if(animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Idle", false);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+        }
+
         animator.SetBool("Walk", !animator.GetBool("Walk"));
     }
 
     public void Idle()
     {
+        animator.SetBool("Idle", true);
         animator.SetBool("Attack", false);
         animator.SetBool("Walk", false);
     }
