f4dd34e1c24c85dd0c95e009c6655988c4020fc9
SP-MICOM <superfamicom1022@gmail.com>
2026-05-27 21:43:37 +0900

    (2026.5.27) 프리팹 시스템

--- Program1/Assets/Class6th (Input System)/Scripts/Character.cs
+++ Program1/Assets/Class6th (Input System)/Scripts/Character.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public Vector3 direction;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed;
 
     private void Start()
     {
         transform.position = new Vector3(1, 1, 1);
+
+        speed = 25;
     }
 
     private void Update()
     {
-        direction.x += Input.GetAxis("Horizontal");
-        direction.z += Input.GetAxis("Vertical");
+        direction.x += Input.GetAxisRaw("Horizontal");
+        direction.z += Input.GetAxisRaw("Vertical");
 
-        transform.position = transform.position + direction;
+        // Time.deltaTime
+        // 이전 프레임에서 현재 프레임까지 걸린 시간입니다.
+
+        direction.Normalize();
+
+        transform.position = transform.position + direction * Time.deltaTime * speed;
     }
 
 }
