da942cfeaeddde481d1d9a4763a2b5a5bf464892
SP-MICOM <superfamicom1022@gmail.com>
2026-06-03 21:44:24 +0900

    (2026.6.3) 코루틴

--- Program1/Assets/Class9th (Coroutine)/Scripts/CreateManager.cs
+++ Program1/Assets/Class9th (Coroutine)/Scripts/CreateManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,16 +7,42 @@ public class CreateManager : MonoBehaviour
 {
     [SerializeField] GameObject elemental;
     [SerializeField] float offset = 1.5f;
+    [SerializeField] List<GameObject> clone;
+    private float time;
+    private int count;
 
     private void Start()
     {
         for (int i = 0; i < 5; i++)
         {
-            GameObject[] clone = new GameObject[5];
-
-            clone[i] = Instantiate(elemental, transform);
+            clone.Add(Instantiate(elemental, transform));
 
             clone[i].transform.position = new Vector3(-3 + (1.5f * i), 0, 0);
+
+            clone[i].SetActive(false);
         }
+
+        StartCoroutine(Coroutine());
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (count < clone.Count && time >= 5.0f)
+        {
+            clone[count++].SetActive(true);
+
+            time = 0.0f;
+        }
+    }
+
+    IEnumerator Coroutine()
+    {
+        Debug.Log("Start Coroutine");
+
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("Stop Coroutine");
     }
 }
