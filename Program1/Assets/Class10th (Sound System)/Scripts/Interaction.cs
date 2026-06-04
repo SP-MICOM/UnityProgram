9d33c885751a569f402f8462a987b4485434295c
SP-MICOM <superfamicom1022@gmail.com>
2026-06-04 21:43:41 +0900

    (2026.6.4) 광원 시스템

---
+++ Program1/Assets/Class10th (Sound System)/Scripts/Interaction.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+
+    private void OnDrawGizmos()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Gizmos.DrawRay(ray.origin, ray.direction * 100);
+    }
+}
--- Program1/Assets/Class9th (Coroutine)/Scripts/CreateManager.cs
+++ Program1/Assets/Class9th (Coroutine)/Scripts/CreateManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CreateManager : MonoBehaviour
+public class CreateManager : MonoBehaviour 
 {
     [SerializeField] GameObject elemental;
     [SerializeField] float offset = 1.5f;
     [SerializeField] List<GameObject> clone;
-    private float time;
+    [SerializeField] WaitForSeconds waitForSeconds = new WaitForSeconds(5.0f);
     private int count;
 
     private void Start()
@@ -17,7 +17,7 @@ public class CreateManager : MonoBehaviour
         {
             clone.Add(Instantiate(elemental, transform));
 
-            clone[i].transform.position = new Vector3(-3 + (1.5f * i), 0, 0);
+            clone[i].transform.position = new Vector3(-3 + (offset * i), 0, 0);
 
             clone[i].SetActive(false);
         }
@@ -25,24 +25,16 @@ public class CreateManager : MonoBehaviour
         StartCoroutine(Coroutine());
     }
 
-    private void Update()
-    {
-        time += Time.deltaTime;
-
-        if (count < clone.Count && time >= 5.0f)
-        {
-            clone[count++].SetActive(true);
-
-            time = 0.0f;
-        }
-    }
-
     IEnumerator Coroutine()
     {
-        Debug.Log("Start Coroutine");
+        while(count < clone.Count)
+        {
+            if (count < clone.Count)
+            {
+                clone[count++].SetActive(true);
+            }
 
-        yield return new WaitForSeconds(5.0f);
-
-        Debug.Log("Stop Coroutine");
+            yield return waitForSeconds;
+        }
     }
 }
