6dd143d861323008c98253127ade5ba4b85426b5
SP-MICOM <superfamicom1022@gmail.com>
2026-06-05 21:42:52 +0900

    (2026.6.5) 카메라 시스템

--- Program1/Assets/Class10th (Sound System)/Scripts/Interaction.cs
+++ Program1/Assets/Class10th (Sound System)/Scripts/Interaction.cs
@@ -1,13 +1,35 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
     [SerializeField] Ray ray;
+    [SerializeField] RaycastHit raycastHit;
+    [SerializeField] float distance = 100.0f;
+    [SerializeField] LayerMask layerMask;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out raycastHit, distance, layerMask))
+            {
+                Debug.Log(raycastHit.collider.gameObject.name);
+
+                AudioSource audioSource = raycastHit.collider.gameObject.GetComponent<AudioSource>();
+
+                if(audioSource.isPlaying == false)
+                {
+                    audioSource.Play();
+                }
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         Gizmos.DrawRay(ray.origin, ray.direction * 100);
     }
 }
